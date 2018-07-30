912f3d845095b83365cba9f1c6b0fa79b137b55d
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-07-30 16:39:16 +0600

    skip before action for GraphQL controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   has_many :prices, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
   accepts_nested_attributes_for :prices,
                 :reject_if => proc { |attrs| attrs['label'].blank? || attrs['amount'].blank? }
   belongs_to :user
