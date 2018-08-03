4178d2f8065e9a209dd95b50dca4e8faf88af7e2
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-03 18:38:25 +0600

    Added uniqueness scope for users and events in Participation

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,10 @@
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :user_id, presence: true
+  validates :event_id, presence: true
+
+  validates_uniqueness_of :user_id, :scope => [:event_id]
+  
 end
