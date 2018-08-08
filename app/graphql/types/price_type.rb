2729ac97fe2772fe57b82ce01cbae21f3ff98dd5
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-08 12:37:35 +0600

    price label can return null value

--- app/graphql/types/price_type.rb
+++ app/graphql/types/price_type.rb
@@ -2,7 +2,7 @@ Types::PriceType = GraphQL::ObjectType.define do
   name 'Price'
 
   field :id, !types.ID
-  field :label, !types.String
+  field :label, types.String
   field :amount, !types.Int
   field :event, -> { Types::EventType }
   
