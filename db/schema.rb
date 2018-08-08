7d83e0c6d715020a01b8e292601d38a451cc7b74
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-08 15:11:52 +0600

    Geocoder added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_122743) do
+ActiveRecord::Schema.define(version: 2018_08_08_081552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2018_08_06_122743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
