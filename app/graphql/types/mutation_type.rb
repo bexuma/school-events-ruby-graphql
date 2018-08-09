f4ff2e0b7c5e38fce2a2d400b744f35975692a80
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-09 14:44:46 +0600

    create && delete Review mutations added

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,8 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :createUser, function: Resolvers::CreateUser.new
   field :signInUser, function: Resolvers::SignInUser.new
   field :createEvent, function: Resolvers::CreateEvent.new
+  field :createReview, function: Resolvers::CreateReview.new
+  field :deleteReview, function: Resolvers::DeleteReview.new
   field :createParticipation, function: Resolvers::CreateParticipation.new
   field :deleteParticipation, function: Resolvers::DeleteParticipation.new
 
