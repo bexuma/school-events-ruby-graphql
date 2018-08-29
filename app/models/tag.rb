8674538959be0f7b33556d2ab197d4feec4f98cb
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-29 19:28:12 +0600

    User tagging added

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,9 @@ class Tag < ApplicationRecord
   has_many :event_taggings
   has_many :events, through: :event_taggings
 
+  has_many :user_taggings
+  has_many :users, through: :user_taggings
+
   before_save { name.downcase! }
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
