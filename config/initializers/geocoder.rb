95d02a082c68a170349e5a970ac41581a6ea8cd3
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-07-22 20:21:24 +0500

    chore: use env variable

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,7 +6,7 @@ Geocoder.configure(
   use_https: false,           # use HTTPS for lookup requests? (if supported)
   http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "AIzaSyBY-lCWTYNp7yRpto9H8vKWDxdMRkYPIVo",               # API key for geocoding service
+  api_key: ENV.fetch("GEOCODER_API_KEY")  # API key for geocoding service
   cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
